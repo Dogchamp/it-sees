59522c6c56afc0f94526199b7ca4cbcf8dc0878e
Chris Quion <chrischrisquion@gmail.com>
2018-03-28 14:47:30 -0700

    added transparent layer feature

--- src/project3/autostereogram.py
+++ src/project3/autostereogram.py
@@ -36,6 +36,7 @@ def make_autostereogram(depth_map, texture, scale):
     colored = np.zeros((height, width), dtype=bool)
     sgram = np.zeros((height, width, 3), np.uint8)
     for i in range(height):
+        print(" row %d " % i)
         for j in range(width):
             color = texture[i%texture.shape[0]][j%texture.shape[1]]
             if j < width:
@@ -50,57 +51,38 @@ def make_autostereogram(depth_map, texture, scale):
                 while j < width and colored[i][j] == True:
                     j += 1  # Get first position j for row i that is uncolored
 
-    t_asgram = make_transparent_autostereogram(depth_map, texture, int(scale/2))
     return sgram
 
-def make_transparent_autostereogram(depth_map, texture, scale):
-    height, width = depth_map.shape[0], depth_map.shape[1]
-    pixel_count = 0
-    colored = np.zeros((height, width), dtype=bool)
-    sgram = np.zeros((height, width, 4), np.uint8)      # 4th channel is the alpha Channel
-    for i in range(height):
-        for j in range(width):
-            color = texture[i%texture.shape[0]][j%texture.shape[1]]
-            if j < width:
-                sgram[i][j] = (color[0], color[1], color[2], 1)
-                colored[i][j] = True
-                z = depth_map[i][j]
-                d = (z + 100) * scale                   # Use a smaller scale than used in non-transparent
-                j = j + d
-            if j >= width:
-                j = 0
-                while j < width and colored[i][j] == True:
-                    j += 1  # Get first position j for row i that is uncolored
-
-    # Make it transparent by going through the sgram and setting every 5th pixel to visible
-    # And all others to transparent
-    for i in range(height):
-        for j in range(width):
-            if pixel_count % 150 == 0:
-                sgram[i][j] = (sgram[i][j][0], sgram[i][j][1], sgram[i][j][2], 1)
-            else:
-                sgram[i][j] = (222, 222, 222, 0.0)
-
-    show_image(sgram, "transparent")
-    return sgram
+def put_overlay(image, overlay, alpha):
+    image = cv2.addWeighted(image, alpha, overlay, .75, -20)
+    return image
 
 
-
-
+# ========== IO ===================
 def show_image(image, description):
     cv2.imshow(description, image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-# ========== MAIN ==========
+# =========== ========== ========= ========== ========== MAIN ========== ========== ========== ========== ==========
 dm_name = 'shark-dm'
 dm_file_type = '.jpg'
+
 dm, h, w = load_depth_map(dm_name + dm_file_type)       # Load the depth map
 show_image(dm, 'depth map')
 texture = make_texture(shape=(int(h/10), int(w/10)))    # Make a random color texture for reference in autostereogram creation
 show_image(texture, "texture")
 
-sgram = make_autostereogram(dm, texture, 1)                # Make the autostereogram
+print( "MAKING AUTOSTEREOGRAM" )
+sgram = make_autostereogram(dm, texture, 2)                # Make the autostereogram
 show_image(sgram, 'sgram')
-cv2.imwrite(dm_name + '-sgram.jpg', sgram)
+
+
+print( "MAKING OVERLAY" )
+overlay = make_autostereogram(dm, texture, .1)             # Make another, but with a smaller shift and transparent
+sgram_with_overlay = put_overlay(sgram, overlay, 0.4)      # and stack em and show em
+show_image(sgram, 'sgram with overlay')
+
+cv2.imwrite(dm_name + '-sgram.jpg', sgram)                 # Save em
 cv2.imwrite('texture.jpg', texture)
+cv2.imwrite(dm_name + '-sgram-with-overlay.jpg', sgram_with_overlay)
